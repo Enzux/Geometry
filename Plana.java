9437f19f1030a47be64a8e92e6d6eff5eb2c319b
Ebyyye <106695473+EllenVargas@users.noreply.github.com>
2024-06-06 12:25:22 -0300

    não pode negativo

--- Plana.java
+++ Plana.java
@@ -13,6 +13,11 @@ public class Plana{
         System.out.println("///////Area do quadrado///////");
         System.out.println("Digite o tamanho da aresta:");
         a = sc1.nextFloat();
+        if (a < 0) {
+            System.out.println("Não pode incluir números negativos");
+            quadrado();
+            System.out.println("\n");
+        }
         return a*a;
     }
     public float retangulo(){
@@ -21,6 +26,11 @@ public class Plana{
         a = sc1.nextFloat();
         System.out.println("Digite o tamanho da base:");
         b = sc1.nextFloat();
+        if (a < 0 || b < 0) {
+            System.out.println("Não pode incluir números negativos");
+            retangulo();
+            System.out.println("\n");
+        }
         return a*b;
     }
     public float circulo(){
@@ -29,6 +39,11 @@ public class Plana{
         System.out.println("Digite o raio do circulo:");
         r = sc1.nextFloat();
         double areaC = (r* 2)* Math.PI;
+         if (r < 0) {
+            System.out.println("Não pode incluir números negativos");
+            circulo();
+            System.out.println("\n");
+        }
         return (float)areaC;
     }
     public float trapezio(){
@@ -39,6 +54,11 @@ public class Plana{
         b = sc1.nextFloat();
         System.out.println("Digite a altura:");
         a = sc1.nextFloat();
+        if (B < 0 || b < 0 || a < 0) {
+            System.out.println("Não pode incluir números negativos");
+            trapezio();
+            System.out.println("\n");
+        }
         return ((B + b)* a)/2;
     }
     public float losangulo(){
@@ -47,6 +67,11 @@ public class Plana{
         D = sc1.nextFloat();
         System.out.println("Digite a diagonal menor:");
         d = sc1.nextFloat();
+        if (a < 0) {
+            System.out.println("Não pode incluir números negativos");
+            losangulo();
+            System.out.println("\n");
+        }
         return (D*d)/2;
     }
     public float triangulo(){
@@ -55,6 +80,11 @@ public class Plana{
         a = sc1.nextFloat();
         System.out.println("Digite o tamanho da base:");
         b = sc1.nextFloat();
+        if (a < 0) {
+            System.out.println("Não pode incluir números negativos");
+            triangulo();
+            System.out.println("\n");
+        }
         return (a*b)/2;
     }
     
